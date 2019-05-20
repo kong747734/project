0aaa498bbafbae50f4726860211b43abb66a2290
赵耿忠 <zhaogengzhong@lighthin.cn>
2019-05-20 16:42:03 +0800

    project

---
+++ app/src/main/java/com/xfhl/wawa/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.xfhl.wawa.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void tiao(View v) {
+//        Intent intent=new Intent(this, Model1Actvity.class);
+//        startActivity(intent);
+    }
+}
